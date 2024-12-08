bf30b6e293060963e8cd35927399e916a6c431a9
gonmpr <gmporrorosas@gmail.com>
2024-12-08 13:54:16 -0300

    Add player.py, circleshape.py; render player in the screen

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
+    #delta-time: amount of time between each frame
+    dt = 0
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 
     while True:
+
+        # Close the window with the close button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+
+        
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
