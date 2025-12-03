28bda0943da0a2ef6c0dd82d7942bb7bc491c411
¨gonmpr¨ <gmporrorosas@gmail.com>
2025-12-02 22:10:00 -0300

    free movement

--- constants.py
+++ constants.py
@@ -1,7 +1,13 @@
+# screen properties
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# player drawing and collision
 PLAYER_RADIUS = 20
 LINE_WIDTH = 2
+
+# player movement speed
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
+
 
